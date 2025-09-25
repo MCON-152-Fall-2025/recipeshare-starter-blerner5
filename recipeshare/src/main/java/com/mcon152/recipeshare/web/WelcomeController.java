06b51daa2cc1bd5469e971b942228a1a180de2f3
lagishtein <lagishtein@gmail.com>
2025-09-25 11:39:15 -0400

    RSF1F2-18 - Add Recipe model

--- recipeshare/src/main/java/com/mcon152/recipeshare/web/WelcomeController.java
+++ recipeshare/src/main/java/com/mcon152/recipeshare/web/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class WelcomeController {
     @GetMapping("/")
     public String home() {
         return "Welcome to RecipeShare!";
